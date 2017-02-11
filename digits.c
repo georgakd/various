49f0b669296bcb988b96f333dbfb047bde6445a4
Dimitra Georgakaki <georgakd@gmail.com>
2017-02-11 21:02:50 +0200

    calc digits of a number

---
+++ digits.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+
+int digits(int N){
+	
+	int result = 0;
+	
+	while (N>0){
+		
+	N = N /10;
+	result+=1;		
+	
+	}
+
+return result;	
+}
+
+int main(int argc, char *argv[]){
+
+    int number,num_digits;
+	number=atoi(argv[1]);
+	num_digits=digits(number);
+
+    printf("The number of digits for number %d is: %d\n",number,num_digits);
+	return 0;
+}
