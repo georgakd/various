10c3af89bd61589527b1d300ae77505002074351
Dimitra Georgakaki <georgakd@gmail.com>
2017-02-11 21:01:46 +0200

    factorial calc

---
+++ factorial.c
@@ -0,0 +1,35 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+
+int factorial(int N){
+	
+	int fact=1;
+	
+	for (int i=1;i<N+1;i++){
+		
+		fact*=i;
+		
+		}
+	
+	
+	return fact;
+	}
+
+
+
+int main(int argc,char **argv){
+
+    int numbers, result;
+	numbers=atoi(argv[1]);
+	result = factorial(numbers);
+	printf("The factorial of %d numbers is: %d\n",numbers,result);	
+	
+	return 0;
+	}
